0599ccb45724484fb2a30f8c0e33dcea018c1ade
chris <cclamb@cs.unm.edu>
2012-03-06 09:55:30 -0700

    proxy settings for apt

--- cookbooks/proxy/metadata.rb
+++ cookbooks/proxy/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures proxy"
+maintainer       "Chris Lamb"
+maintainer_email "chrislambistan@gmail.com"
+license          "BSD"
+description      "Installs/Configures proxy for APT"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
